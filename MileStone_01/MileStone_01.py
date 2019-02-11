bedd432e39e7756fa66aeeb7b2814a63cf2c4533
OceansBlue <OceansBlueCode@gmail.com>
2019-02-10 22:15:31 -0600

    02-10-2019 more code

--- MileStone_01/MileStone_01.py
+++ MileStone_01/MileStone_01.py
@@ -9,22 +9,32 @@
 -- Find Movies
 -- Stop running the program
 
+
 Tasks:
-[]: Decide where to store movies
+[x]: Decide where to store movies
 []: What is the format of the movie
-[]: Show the user the main interface and get their input
+[x]: Show the user the main interface and get their input
 []: Allow users to add movies
 []: Show all their movies
 []: Find a movie
-[]: Stop running the program when they type 'q'
+[x]: Stop running the program when they type 'q'
 """
 
 
+movies = []    # ''' list to store movies '''
+
+'''
+movies = {
+    'name' = ...(str),
+    'director' = ...(str),
+    'year' = ...(int)
+    }
+'''
 
 def menu():
     user_input = input("Enter 'a' to add a movie, 'l' to see your list of movies, 'f' to find a movie. 'q' to quit")
-
-    While user_input != 'q':
+    
+    while user_input != 'q':
         if user_input == 'a':
            add_movie()
         elif user_input == 'l':
@@ -36,6 +46,31 @@ def menu():
     ''' elif user_input =='q':
         print("Stopping Program....") '''
 
-        user_input = input("Enter 'a' to add a movie, 'l' to see your list of movies, 'f' to find a movie. 'q' to quit")
+    user_input = input("Enter 'a' to add a movie, 'l' to see your list of movies, 'f' to find a movie. 'q' to quit")
+
+
+def add_movie():
+    name = input('Enater the movie name:  ')
+    director = input("Enter the movie director's name: ")
+    year = int((input('The year movie produced: '))
+
+    movies.append({    # we did add the dictionary inside the append function. appending the dictionary to my list.
+        'name': name,
+        'director': director,
+        'year': year
+    })
+
+"""
+                    OR we can do it like this:
+                                movie = {
+                                    'name': name,
+                                    'director': director,
+                                    'year': year
+                                    }
+                                movie.append(movie)
+"""
 
 menu()
+
+print(movies)
+
